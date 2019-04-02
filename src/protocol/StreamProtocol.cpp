4b227d90802735f872671936a65915fa6f91277d
Jeff Rowberg <jeff@rowberg.net>
2019-04-02 14:10:24 -0400

    Add StreamProtocol stub implementations

--- src/protocol/StreamProtocol.cpp
+++ src/protocol/StreamProtocol.cpp
@@ -3,4 +3,24 @@
 namespace Perilib
 {
 
+int8_t StreamProtocol::testPacketStart(const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator, bool isTx)
+{
+    return ParseStatus::IN_PROGRESS;
+}
+
+int8_t StreamProtocol::testPacketComplete(const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator, bool isTx)
+{
+    return ParseStatus::COMPLETE;
+}
+
+int8_t StreamProtocol::getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false)
+{
+    return 0;
+}
+
+int8_t StreamProtocol::getPacketFromNameAndArgs(StreamPacket *packet, const char *packetName, StreamParserGenerator *parserGenerator=0, ...)
+{
+    return 0;
+}
+
 } // namespace Perilib
--- src/protocol/StreamProtocol.h
+++ src/protocol/StreamProtocol.h
@@ -9,6 +9,14 @@ namespace Perilib
     
 class StreamProtocol : public Protocol
 {
+public:
+    virtual int8_t testPacketStart(const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
+    virtual int8_t testPacketComplete(const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
+    virtual int8_t getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
+    virtual int8_t getPacketFromNameAndArgs(StreamPacket *packet, const char *packetName, StreamParserGenerator *parserGenerator=0, ...);
+    
+    uint32_t incomingPacketTimeout;
+    uint32_t responsePacketTimeout;
 };
 
 } // namespace Perilib
