6337406de71221664ab795bb210315a9caa34385
Jeff Rowberg <jeff@rowberg.net>
2019-04-18 23:35:30 -0400

    Add Text_Demo example

---
+++ examples/Text_Demo/Text_Demo.ino
@@ -0,0 +1,23 @@
+#include <Perilib.h>
+
+Perilib::TextStreamProtocol protocol;
+Perilib::StreamParserGenerator parser(&protocol);
+
+const uint8_t textPacket1[] = "TEST COMMAND 1\r\n";
+const uint8_t textPacket2[] = "TEST ERR" "\x08\x08\x08" "COMMAND 2\r\n";
+const uint8_t textPacket3[] = "TEST 3\n";
+
+void setup() {
+  // initialize host serial interface for monitoring
+  Serial.begin(9600);
+}
+
+void loop() {
+  // parse example packets
+  parser.parse(textPacket1, sizeof(textPacket1) - 1);
+  parser.parse(textPacket2, sizeof(textPacket2) - 1);
+  parser.parse(textPacket3, sizeof(textPacket3) - 1);
+
+  // wait between iterations
+  delay(5000);
+}
