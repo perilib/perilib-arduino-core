36d8101ae8277d9c31735ad23015d211f468a48f
Jeff Rowberg <jeff@rowberg.net>
2019-04-18 23:35:17 -0400

    Add getPacketFromBuffer stub for TextStreamProtocol

--- src/TextStreamProtocol.cpp
+++ src/TextStreamProtocol.cpp
@@ -7,4 +7,12 @@ const uint8_t TextStreamProtocol::textBackspaceBytes[2] = {0x08, 0x7F};
 const uint8_t TextStreamProtocol::textTerminalBytes[1] = {0x0A};
 const uint8_t TextStreamProtocol::textTrimBytes[2] = {0x0A, 0x0D};
 
+int8_t TextStreamProtocol::getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator, bool isTx)
+{
+    Serial.print("PACKET: [ ");
+    Serial.write(buffer, length);
+    Serial.println(" ]");
+    return 0;
+}
+
 } // namespace Perilib
--- src/TextStreamProtocol.h
+++ src/TextStreamProtocol.h
@@ -20,6 +20,8 @@ public:
         trimBytes = textTrimBytes;
     }
 
+    virtual int8_t getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator=0, bool isTx=false);
+
 protected:
     static const uint8_t textBackspaceBytes[2];
     static const uint8_t textTerminalBytes[1];
