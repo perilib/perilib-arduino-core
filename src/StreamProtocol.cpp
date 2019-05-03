96ae0435fb08468d907be23d8fd8a3da31fbbb86
Jeff Rowberg <jeff@rowberg.net>
2019-05-02 23:27:19 -0400

    Convert empty stub base methods to pure virtual

--- src/StreamProtocol.cpp
+++ src/StreamProtocol.cpp
@@ -32,16 +32,6 @@ int8_t StreamProtocol::testPacketComplete(const uint8_t *buffer, uint16_t length
     return ParseStatus::COMPLETE;
 }
 
-int8_t StreamProtocol::getPacketFromBuffer(StreamPacket *packet, const uint8_t *buffer, uint16_t length, StreamParserGenerator *parserGenerator, bool isTx)
-{
-    return 0;
-}
-
-int8_t StreamProtocol::getPacketFromNameAndArgs(StreamPacket *packet, const char *packetName, StreamParserGenerator *parserGenerator, ...)
-{
-    return 0;
-}
-
 uint8_t *StreamProtocol::getNextArgument(uint8_t *argDef)
 {
     // assume each argument is a single byte
