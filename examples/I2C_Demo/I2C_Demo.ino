f79d0db1d70f6626127b23b0e0caa47806760592
Jeff Rowberg <jeff@rowberg.net>
2019-08-17 21:53:53 -0400

    Add I2C_Demo for basic register interface usage

---
+++ examples/I2C_Demo/I2C_Demo.ino
@@ -0,0 +1,34 @@
+#include <Perilib.h>
+
+uint8_t rxBuffer[512];
+uint8_t txBuffer[512];
+Perilib::TwiRegisterInterface_ArduinoWire twi(0x68, &Wire);
+
+void setup() {
+  // initialize host serial interface for monitoring
+  Serial.begin(9600);
+  
+  // initialize Wire interface
+  Wire.begin();
+
+  // initialize TX buffer with recognizable pattern
+  int i;
+  for (i = 0; i < 512; i++) txBuffer[i] = i & 0xFF;
+}
+
+void loop() {
+  // write 512 bytes to register 0x6F
+  int txResult = twi.writeBytes(0x6F, txBuffer, 512);
+  Serial.print("TX result: ");
+  Serial.print(txResult);
+  Serial.println(" bytes written out of 512 attempted");
+
+  // read 512 bytes from register 0x6F
+  int rxResult = twi.readBytes(0x6F, rxBuffer, 512);
+  Serial.print("RX result: ");
+  Serial.print(rxResult); Serial.print(", ");
+  Serial.println(" bytes read out of 512 requested");
+
+  // wait between iterations
+  delay(5000);
+}
