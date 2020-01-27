a1b2469a29d85c46d1318f027565194271a80899
Jeff Rowberg <jeff@rowberg.net>
2020-01-26 22:14:21 -0500

    Make Register_TWI_read_write example a bit more generic

--- examples/Register_TWI_read_write/Register_TWI_read_write.ino
+++ examples/Register_TWI_read_write/Register_TWI_read_write.ino
@@ -2,11 +2,14 @@
 
 uint8_t rxBuffer[512];
 uint8_t txBuffer[512];
-Perilib::TwiRegisterInterface_ArduinoWire twi(0x68, &Wire);
+Perilib::TwiRegisterInterface_ArduinoWire twiSlave(0x04, &Wire);
 
 void setup() {
   // initialize host serial interface for monitoring
   Serial.begin(9600);
+  
+  // wait for enumeration for USB-based serial interface (optional)
+  //while (!Serial);
   
   // initialize Wire interface
   Wire.begin();
@@ -17,18 +20,18 @@ void setup() {
 }
 
 void loop() {
-  // write 512 bytes to register 0x6F
-  int txResult = twi.writeBytes(0x6F, txBuffer, 512);
+  // write 512 bytes to register 0x01
+  int txResult = twiSlave.writeBuf_reg8(0x01, txBuffer, 512);
   Serial.print("TX result: ");
   Serial.print(txResult);
   Serial.println(" bytes written out of 512 attempted");
 
-  // read 512 bytes from register 0x6F
-  int rxResult = twi.readBytes(0x6F, rxBuffer, 512);
+  // read 512 bytes from register 0x02
+  int rxResult = twiSlave.readBuf_reg8(0x02, rxBuffer, 512);
   Serial.print("RX result: ");
   Serial.print(rxResult);
   Serial.println(" bytes read out of 512 requested");
-
+  
   // wait between iterations
   delay(2000);
 }
